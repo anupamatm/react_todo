55981ac5de2ec79f482e45d0020c21668f429f0f
anupama <anupamaunni612@gmail.com>
2024-03-14 15:35:24 +0530

    changed from useState to useReducer

--- src/components/Todo1.jsx
+++ src/components/Todo1.jsx
@@ -1,6 +1,21 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import List from './List';
 import Addtodo1 from './Addtodo1';
+
+function todoReducer(state,action){
+ switch(action.type){
+  case 'ADD_TODO':
+    return [...state, action.payload];
+  case 'DELETE_TODO':
+    return state.filter(todo => todo.id !== action.payload);
+  case 'COMPLETE_TODO':
+    return state.map(todo =>
+      todo.id === action.payload ? { ...todo, completed: true } : todo
+    );
+  default:
+    return state;
+ }
+}
 
 function Todo1() {
 
@@ -10,31 +25,28 @@ function Todo1() {
     { id: 3, text: "Walk the dog", completed: true },
     { id: 4, text: "Clean the house", completed: false }
   ];
-const [todoList,setTodoList]=useState(todos)
+const [todoList, dispatch]=useReducer(todoReducer,todos)
+// const [todoList,setTodoList]=useState(todos)
 
   const onitemAdd=(newItem)=>{
-      console.log(newItem)
-      const newList=[...todoList,newItem]
-      setTodoList(newList)
+      
+      dispatch({
+        type:'ADD_TODO',
+        payload:newItem
+      })
   }
   const onDelete=(id)=>{
-    console.log(id)
-    const newList= todoList.filter((item)=>{
-      return item.id!==id
+    dispatch({
+      type:'DELETE_TODO',
+      payload:id
     })
-    setTodoList(newList)
+    
   }
   const onCompleate = (id) => {
-    console.log(id)
-    const updatedList = todoList.map(todo => {
-      if (todo.id === id) {
-        // Create a copy of the todo item with the completed property updated
-        return { ...todo, completed: true };
-      }
-      return todo; // Return the original todo item for other IDs
-    });
-    // console.log(updatedList)
-    setTodoList(updatedList); // Update the todoList state with the new array
+    dispatch({
+      type:'COMPLETE_TODO',
+      payload:id
+    })
   };
   
   
